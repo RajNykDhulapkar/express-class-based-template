30a5c37e59295e264b03e80f08eda62aa9929c9a
RajNykDhulapkar <rajdhulapkar3@gmail.com>
2022-12-02 21:03:55 +0530

    -a

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,56 @@
-import * as bodyParser from 'body-parser';
-import * as cookieParser from 'cookie-parser';
-import * as express from 'express';
-import * as mongoose from 'mongoose';
-import Controller from './interfaces/controller.interface';
-import errorMiddleware from './middleware/error.middleware';
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as express from "express";
+import * as mongoose from "mongoose";
+import Controller from "./interfaces/controller.interface";
+import errorMiddleware from "./middleware/error.middleware";
 
 class App {
-  public app: express.Application;
+    public app: express.Application;
 
-  constructor(controllers: Controller[]) {
-    this.app = express();
+    constructor(controllers: Controller[]) {
+        this.app = express();
 
-    this.connectToTheDatabase();
-    this.initializeMiddlewares();
-    this.initializeControllers(controllers);
-    this.initializeErrorHandling();
-  }
+        this.connectToTheDatabase();
+        this.initializeMiddlewares();
+        this.initializeControllers(controllers);
+        this.initializeErrorHandling();
+    }
 
-  public listen() {
-    this.app.listen(process.env.PORT, () => {
-      console.log(`App listening on the port ${process.env.PORT}`);
-    });
-  }
+    public listen() {
+        this.app.listen(process.env.PORT, () => {
+            console.log(`App listening on the port ${process.env.PORT}`);
+        });
+    }
 
-  public getServer() {
-    return this.app;
-  }
+    public getServer() {
+        return this.app;
+    }
 
-  private initializeMiddlewares() {
-    this.app.use(bodyParser.json());
-    this.app.use(cookieParser());
-  }
+    private initializeMiddlewares() {
+        this.app.use(bodyParser.json());
+        this.app.use(cookieParser());
+    }
 
-  private initializeErrorHandling() {
-    this.app.use(errorMiddleware);
-  }
+    private initializeErrorHandling() {
+        this.app.use(errorMiddleware);
+    }
 
-  private initializeControllers(controllers: Controller[]) {
-    controllers.forEach((controller) => {
-      this.app.use('/', controller.router);
-    });
-  }
+    private initializeControllers(controllers: Controller[]) {
+        controllers.forEach((controller) => {
+            this.app.use("/", controller.router);
+        });
+    }
 
-  private connectToTheDatabase() {
-    const {
-      MONGO_USER,
-      MONGO_PASSWORD,
-      MONGO_PATH,
-    } = process.env;
-    mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`);
-  }
+    private connectToTheDatabase() {
+        try {
+            const { MONGO_USER, MONGO_PASSWORD, MONGO_PATH, DATABASE_URI } = process.env;
+            mongoose.connect(DATABASE_URI);
+            console.log("Connected to database");
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 export default App;
